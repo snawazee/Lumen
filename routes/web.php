8e31b0ababd59ebfd2988668f61a8abf7e9ed6b0
unknown <snawazee@gmail.com>
2020-10-19 16:13:08 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,8 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->get('authors',  ['uses' => 'AuthorController@showAllAuthors']);
-  
     $router->get('authors/{id}', ['uses' => 'AuthorController@showOneAuthor']);
-  
     $router->post('authors', ['uses' => 'AuthorController@create']);
-  
     $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
-  
     $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
   });
