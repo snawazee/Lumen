e766ca7fca5b6da361823062bb4bd7c6d40bee33
unknown <snawazee@gmail.com>
2020-10-19 19:52:00 +0530

    changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,12 +10,10 @@ class UserController extends Controller
     {
         return response()->json(User::all());
     }
-
     public function showOneUser($id)
     {
         return response()->json(User::find($id));
     }
-
     public function create(Request $request)
     {
         $this->validate($request, [
@@ -30,21 +26,15 @@ class UserController extends Controller
             'country' => 'required|alpha',
             'mobile'  => 'required|unique:users',
         ]);
-
         $user = User::create($request->all());
-
         return response()->json($user, 201);
-        
     } 
-
     public function update($id, Request $request)
     {
         $user = User::findOrFail($id);
         $user->update($request->all());
-
         return response()->json($user, 200);
     }
-
     public function delete($id)
     {
         User::findOrFail($id)->delete();
